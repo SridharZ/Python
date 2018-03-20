9bfb9c21eb2d21b1bbc445a54029556ad12b496c
D <SD035207@cerner.net>
2018-03-20 16:52:42 +0530

    First checkin

---
+++ python -tt script.py
@@ -0,0 +1,23 @@
+import random
+import sys
+import os
+
+
+
+Input = str(input('Enter name'))
+rev = (Input)[::-1] #reverse the string
+if Input == rev:
+    print(rev, 'is an palindrome')
+else:
+    print(rev,'is not palindrome')
+
+
+
+
+
+
+
+
+
+
+
